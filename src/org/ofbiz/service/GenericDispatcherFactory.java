7da3fbb3a81f7a87206c659693baf6a053a0c7b9
jacopoc <jacopoc@13f79535-47bb-0310-9956-ffa450edef68>
2012-07-24 09:16:26 +0000

    A rather important refactoring of service related code: * new factory interface for LocalDispatcher objects * new factory implementation of GenericDispatcher * new ServiceContainer container that initializes the factory (based on a configuration parameter) * moved the getLocalDispatcher method out of GenericDispatcher and refactored (in a thread safe way) into ServiceContainer/GenericDispatcherFactory * ServiceContainer also maintains the cache of LocalDispatchers that was managed by GenericDispatcher * added code to ServiceContainer to deregister LocalDispatchers at shutdown: this finally fix the current code that was broken because the thread pool maintained by the JobPoller was never closed because of an issue with LocalDispatchers not being deregistered when not started by the ContextFilter * change all code to call the ServiceContainer rather than GenericContainer to get a LocalDispatcher: now the implementation is no more directly referenced by calling code
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk/framework/service@1364952 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/ofbiz/service/GenericDispatcherFactory.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.service;
+
+import org.ofbiz.entity.Delegator;
+
+public class GenericDispatcherFactory implements LocalDispatcherFactory {
+    public LocalDispatcher createLocalDispatcher(String name, Delegator delegator) {
+        // attempts to retrieve an already registered DispatchContext with the name "name"
+        LocalDispatcher dispatcher = ServiceDispatcher.getLocalDispatcher(name, delegator);
+        // if not found then create a new GenericDispatcher object; the constructor will also register a new DispatchContext in the ServiceDispatcher with name "dispatcherName"
+        if (dispatcher == null) {
+            dispatcher = new GenericDispatcher(name, delegator);
+        }
+        return dispatcher;
+    }
+}
